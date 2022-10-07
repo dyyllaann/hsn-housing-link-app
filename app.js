3e4389b5e1be505f6f0eed63c6ea4d4dcc610243
dyyllaann <dylan.arveson@gmail.com>
2022-10-06 17:10:14 -0700

    Move submit-application route to applicant_controller.

--- app.js
+++ app.js
@@ -99,32 +99,8 @@ app.post(
 	})
 );
 
-// POST submit-listing page.
-app.post("/submit-listing", (req, res, next) => {
-	const applicant = new Applicant({
-		firstName: req.body.firstName,
-		lastName: req.body.lastName,
-		email: req.body.email,
-		date_added: new Date,
-		tenants: req.body.tenants,
-		seeking: req.body.seeking,
-		preferred_location: req.body.location,
-		bedrooms: req.body.tenants,
-		price: {"min": req.body.priceRange_min, "max": req.body.priceRange_max},
-		// pets: { type: Array }, // Currently using string instead of array or object
-		pets_string: req.body.pets,
-		vehicles: req.body.vehicles,
-		occupation_location: req.body.occupationLocation,
-		story: req.body.story,
-		interests: req.body.interests,
-		status: "pending",
-	}).save((err) => {
-		if (err) {
-			return next(err);
-		}
-		res.redirect("/");
-	});
-});
+/* Applicant Routes */
+app.post("/submit-listing", applicant_controller.submit_listing);
 
 /* Admin Routes */
 app.get("/admin", dashboard_controller.applicants_list);
--- controllers/applicantController.js
+++ controllers/applicantController.js
@@ -10,3 +10,30 @@ exports.applicants_list = function (req, res) {
 			res.render('applicants_list', { title: 'All Applicants', applicants_list: list_applicants, user: req.user });
 		}); 
 };
+
+// POST submit-listing page.
+exports.submit_listing = function (req, res, next) {
+	const applicant = new Applicant({
+		firstName: req.body.firstName,
+		lastName: req.body.lastName,
+		email: req.body.email,
+		date_added: new Date,
+		tenants: req.body.tenants,
+		seeking: req.body.seeking,
+		preferred_location: req.body.location,
+		bedrooms: req.body.tenants,
+		price: {"min": req.body.priceRange_min, "max": req.body.priceRange_max},
+		// pets: { type: Array }, // Currently using string instead of array or object
+		pets_string: req.body.pets,
+		vehicles: req.body.vehicles,
+		occupation_location: req.body.occupationLocation,
+		story: req.body.story,
+		interests: req.body.interests,
+		status: "pending",
+	}).save((err) => {
+		if (err) {
+			return next(err);
+		}
+		res.redirect("/");
+	});
+};
